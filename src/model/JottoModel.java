a54488f8572e7666b2e200fcf0f3731683681543
Carolyn Chang <changc@mit.edu>
2014-04-27 18:03:11 -0400

    implemented problem 1

--- src/model/JottoModel.java
+++ src/model/JottoModel.java
@@ -1,14 +1,51 @@
 package model;
+import java.net.*;
+import java.io.*;
 
 /**
  * TODO Write the specification for JottoModel
  */
 public class JottoModel {
+    private final String puzzleNumber;
+    
+    public JottoModel(String puzzleNumber){
+        this.puzzleNumber = puzzleNumber;
+    }
+    /**
+     * This method receives a client's guess and returns the server's response.
+     * 
+     * @param guess, the user's guess of what the word is
+     * @return the server's response
+     * @throws IOException 
+     */
+    
+    public String makeGuess(String guess) throws IOException {
+        // Problem 1
+        URL oracle = createURLrequest(guess);
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(oracle.openStream())
+        );
+        
+        String output = "";
+        String inputLine;
+        while ((inputLine = in.readLine()) != null)
+            output += inputLine;
+        in.close();
+        return output;
+    }
     
     /**
-     * TODO Write the specification for makeGuess
+     * This method creates the correct URL request using the client's puzzle Number and guess.
+     * @param guess, the user's guess of what the word is
+     * @return the URL request to send to the sever
+     * @throws MalformedURLException
      */
-    public void makeGuess(String guess) {
-        // TODO Problem 1
+    private URL createURLrequest(String guess) throws MalformedURLException{
+        String URLrequest = "http://courses.csail.mit.edu/6.005/jotto.py?puzzle="
+                + puzzleNumber 
+                + "&guess=" 
+                + guess;
+        
+        return new URL(URLrequest);
     }
 }
